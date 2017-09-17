7c1932e3adb8ea8f304a8ee966cf9c2f330f8f0f
Martin Jackson <l3tmein.6@gmail.com>
2017-09-16 20:06:11 -0700

    Angular-10 Review

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -18,12 +18,34 @@
          templateUrl: '/templates/directives/seek_bar.html',
          replace: true,
          restrict: 'E',
+         scope: {
+         onChange: '&'
+         }, 
+             
+             
+             
          scope: { },
          link: function(scope, element, attributes) {
              scope.value = 0;
              scope.max = 100;
              
              var seekBar = $(element);
+             
+             /**
+             * @attribute
+             * @
+             */
+             attributes.$observe('value', function(newValue) {
+                scope.value = newValue;
+             });
+ 
+             /**
+             * @attribute
+             * @
+             */
+             attributes.$observe('max', function(newValue) {
+                scope.max = newValue;
+             });
  
              var percentString = function () {
                  var value = scope.value;
@@ -45,6 +67,7 @@
              scope.onClickSeekBar = function(event) {
                 var percent = calculatePercent(seekBar, event);
                 scope.value = percent * scope.max;
+                notifyOnChange(scope.value);
              };
              
              scope.trackThumb = function() {
@@ -52,6 +75,7 @@
                     var percent = calculatePercent(seekBar, event);                   
                     scope.$apply(function() {
                         scope.value = percent * scope.max;
+                        notifyOnChange(scope.value);
                     });
                 });
  
@@ -60,6 +84,12 @@
                     $document.unbind('mouseup.thumb');
                 });
              };
+             
+             var notifyOnChange = function(newValue) {
+                 if (typeof scope.onChange === 'function') {
+                     scope.onChange({value: newValue});
+                 }
+             }; 
              
          }
      };
